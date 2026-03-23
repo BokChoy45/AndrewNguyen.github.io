ca04c0f366c2b2575b952baa8d81a6bcd772f0ae
BokChoy45 <anguyencoco2003@gmail.com>
2026-03-23 09:33:19 +0000

    yes

--- Hw2/Hw2.js
+++ Hw2/Hw2.js
@@ -1,1 +1,25 @@
+function validateForm() {
+    let fname = document.getElementById("fname").value.trim();
+    let midname = document.getElementById("midname").value.trim();
 
+var FnamePattern = /^[a-zA-Z'-]+$/;
+var MidnamePattern = /^[A-Z]+$/;
+
+    if (fname == "") {
+            alert ("Must enter a First Name");
+            return false;
+        } if (!fname.match(FnamePattern)) {
+                alert ("Letters, apostrophes, and dashes ONLY")
+                return false;
+    
+    } if (midname == "") {
+        return true;
+        } if (!midname.match(MidnamePattern)) {
+            alert ("Letters ONLY");
+            return false;
+
+        } else {
+            return true;
+    }
+        
+}
